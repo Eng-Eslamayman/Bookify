53ae65bcd202be241cdc2f387e59c63cd3669a8e
eslam <eslam@ESLAMAYMAN>
2024-02-01 00:13:25 +0200

    - Add Categories form - Add client side validation - add create category view model

--- Bookify.Web/Controllers/CategoriesController.cs
+++ Bookify.Web/Controllers/CategoriesController.cs
@@ -1,4 +1,6 @@
-﻿using Bookify.Web.Data;
+﻿using Bookify.Web.Core.Models;
+using Bookify.Web.Core.ViewModels;
+using Bookify.Web.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Bookify.Web.Controllers
@@ -15,6 +17,23 @@ namespace Bookify.Web.Controllers
         public IActionResult Index()
         {
             return View(_Context.Categories.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(CreateCategoryViewModel model)
+        {
+            if (!ModelState.IsValid)
+            return View(model);
+
+            var category = new Category { Name = model.Name};
+            _Context.Categories.Add(category);
+            _Context.SaveChanges();
+            return RedirectToAction(nameof(Index));  
         }
     }
 }
---
+++ Bookify.Web/Core/ViewModels/CreateCategoryViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Bookify.Web.Core.ViewModels
+{
+    public class CreateCategoryViewModel
+    {
+        [MaxLength(50,ErrorMessage = "Max length cannot be more than 50 chr.")]
+        public string Name { get; set; } = null!;
+
+    }
+}
