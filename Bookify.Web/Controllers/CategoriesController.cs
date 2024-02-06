10b8683bd005ac5054fece236197bc35c9844dd9
eslam <eslam@ESLAMAYMAN>
2024-02-06 16:44:36 +0200

    - rename view model and create view to category form view model  and form view - add edit action

--- Bookify.Web/Controllers/CategoriesController.cs
+++ Bookify.Web/Controllers/CategoriesController.cs
@@ -21,19 +21,48 @@ namespace Bookify.Web.Controllers
 
         public IActionResult Create()
         {
-            return View();
+            return View("Form");
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(CreateCategoryViewModel model)
+        public IActionResult Create(CategoryFormViewModel model)
         {
             if (!ModelState.IsValid)
-            return View(model);
+                return View(model);
 
-            var category = new Category { Name = model.Name};
+            var category = new Category { Name = model.Name };
             _Context.Categories.Add(category);
             _Context.SaveChanges();
-            return RedirectToAction(nameof(Index));  
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var category = _Context.Categories.Find(id);
+            if (category is null)
+                return NotFound();
+
+            var viewModel = new CategoryFormViewModel
+            {
+                Id = id,
+                Name = category.Name,
+            };
+            return View("Form", viewModel);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id ,CategoryFormViewModel model)
+        {
+            if (!ModelState.IsValid)
+              return View("Form",model);
+
+            var category = _Context.Categories.Find(id);
+
+            category.Name = model.Name;
+            category.LastUpdatedOn = DateTime.Now;
+
+            _Context.SaveChanges();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- Bookify.Web/Core/ViewModels/CategoryFormViewModel.cs
+++ Bookify.Web/Core/ViewModels/CategoryFormViewModel.cs
@@ -1,7 +1,8 @@
 ﻿namespace Bookify.Web.Core.ViewModels
 {
-    public class CreateCategoryViewModel
+    public class CategoryFormViewModel
     {
+        public int Id { get; set; }
         [MaxLength(50,ErrorMessage = "Max length cannot be more than 50 chr.")]
         public string Name { get; set; } = null!;
 
