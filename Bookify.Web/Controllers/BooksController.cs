4db089e8598faeb4d86fff329d7d4eb31a64b709
eslam <eslam@ESLAMAYMAN>
2024-02-21 02:55:41 +0200

    - Add Book Form - Add BookController

---
+++ Bookify.Web/Controllers/BooksController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Bookify.Web.Controllers
+{
+    public class BooksController : Controller
+    {
+        readonly ApplicationDbContext _context;
+        readonly IMapper _mapper;
+
+        public BooksController(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public IActionResult Index()
+        {
+           return View();
+        } 
+        public IActionResult Create()
+        {
+            var authors = _context.Authors.Where(a => !a.IsDeleted).OrderBy(a => a.Name).ToList();
+            var categories = _context.Categories.Where(a => !a.IsDeleted).OrderBy(a => a.Name).ToList();
+
+            var viewModel = new BookFormViewModel
+            {
+                Authors = _mapper.Map<IEnumerable<SelectListItem>>(authors),
+                Categories = _mapper.Map<IEnumerable<SelectListItem>>(categories),
+            };
+            return View("Form", viewModel);
+        }
+    }
+}
---
+++ Bookify.Web/Core/ViewModels/BookFormViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Bookify.Web.Core.ViewModels
+{
+    public class BookFormViewModel
+    {
+        public int Id { get; set; }
+        [MaxLength(500)]
+        public string Title { get; set; } = null!;
+        [Display(Name = "Author")]
+        public int AuthorId { get; set; }
+        public Author? Author { get; set; }
+        public IEnumerable<SelectListItem>? Authors { get; set; }
+        [MaxLength(200)]
+        public string Publisher { get; set; } = null!;
+        [Display(Name = "Publishing Date")]
+        public DateTime PublishingDate { get; set; } = DateTime.Now;
+        public IFormFile? Image { get; set; }
+        [MaxLength(200)]
+        public string Hall { get; set; } = null!;
+        [Display(Name = "Is available for rental?")]
+
+        public bool IsAvailableForRental { get; set; }
+        public string Description { get; set; } = null!;
+        public IList<int> SelectedCategories { get; set; } = new List<int>();
+        public IEnumerable<SelectListItem>? Categories { get; set; }
+
+    }
+}
